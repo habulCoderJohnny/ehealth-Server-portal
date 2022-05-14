9bc0341d009bbb952e1cf23d0f4516519490fe1b
habulCoderJohnny <fokruljohnny@gmail.com>
2022-05-15 02:49:44 +0600

    Get my own inserted data to stored server from DB

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 
 //middleware
 app.use(cors());
@@ -14,27 +14,34 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 //console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run() {
-    try {
-        await client.connect();
-        //console.log('db connected!!');
-        const servicesCollection = client.db("doctors_portal").collection("services");
+  async function run() {
+      try{
+          await client.connect();
+          //console.log('db connected!!');
+          const serviceCollection = client.db("doctors_portal").collection("services");
 
+          //GET DATA myOWN Inserted DATA
+          app.get('/service', async(req,res)=>{
+              const query = {};
+              const cursor = serviceCollection.find(query);
+              const services = await cursor.toArray();
+              res.send(services);
+          })
+          
+      }
+      finally{
 
-    }
-    finally {
-
-    }
-
+      }
+    
 }
 run().catch(console.dir);
 
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World from doctors developing portal')
+  res.send('Hello World from doctors developing portal')
 })
 
 app.listen(port, () => {
-    console.log(`Doctor app listening on port ${port}`)
+  console.log(`Doctor app listening on port ${port}`)
 })
